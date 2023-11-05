6cebfa6e58278d607c76b7d7b0b83cc5d34919a2
Moktadir Rahman <autocircled@gmail.com>
2023-11-05 22:51:15 +0600

    route fully works

---
+++ src/pages/index.js
@@ -0,0 +1,8 @@
+import About from './About'
+import Blog from './Blog'
+import Contact from './Contact'
+import Home from './Home'
+import Project from './Project'
+import Service from './Service'
+
+export { About, Blog, Contact, Home, Project, Service }
